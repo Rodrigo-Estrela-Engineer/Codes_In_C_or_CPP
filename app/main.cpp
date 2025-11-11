ab0e873e3cae6f2a66bda674fb877380b6ea4805
Rodrigo Estrela <160602751+estrela801@users.noreply.github.com>
2025-11-11 17:30:35 +0000

    Melhorando a inicialização da API com tratamento de erros

--- app/main.cpp
+++ app/main.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 #include "crow.h"
+#include <fstream>
 using namespace std;
 
 
 int main(){
     crow::SimpleApp app; // criando uma instância do aplicativo
     CROW_ROUTE(app, "/")([]() {
-        return "Hello, World!";
+        try{
+            cout << "Página inicial carregada..." << endl;
+            return "Olá, bem vindo a minha página inicial!";
+        }
+        catch(...){
+            runtime_error("Erro ao carregar a página.");
+            cout << "Erro ao carregar a página." << endl;
+        }
     });
     app.port(18080).multithreaded().run(); // iniciando o servidor na porta 18080
 }
