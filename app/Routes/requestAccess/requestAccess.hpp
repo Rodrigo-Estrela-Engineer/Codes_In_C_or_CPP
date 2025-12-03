6557b5a7eba61d3afdc207eb2d853df40c079b2d
Rodrigo Estrela <160602751+estrela801@users.noreply.github.com>
2025-12-03 14:41:13 +0000

    Alterando nome de referencia da compilação

--- app/Routes/requestAccess/requestAccess.hpp
+++ app/Routes/requestAccess/requestAccess.hpp
@@ -2,8 +2,8 @@
 #include "crow.h"
 #include <stdexcept>
 #include "./receiveJson/receiveJson.hpp"
-#ifndef ROUTES_HPP
-#define ROUTES_HPP
+#ifndef REQUEST_ACCESS_HPP
+#define REQUEST_ACCESS_HPP
 using namespace std;
 void mainRoute(crow::SimpleApp& app);
 #endif // ROUTES_HPP
