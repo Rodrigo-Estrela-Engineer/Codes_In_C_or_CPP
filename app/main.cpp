8da020057ff3b24e390c12b81710bbb7935af7e8
Rodrigo Estrela <160602751+estrela801@users.noreply.github.com>
2025-12-03 16:19:58 +0000

    Adicionando verificação de carregamento de .env

--- app/main.cpp
+++ app/main.cpp
@@ -3,10 +3,30 @@
 #include <fstream>
 #include "./Routes/requestAccess/requestAccess.hpp"
 #include "./Middlewares/Auth/Auth.h"
+#include "./cpp-dotenv/include/dotenv.h" 
 using namespace std;
+// using namespace dotenv;
+string GLOBAL_EXPECTED_API_KEY;
+const string env_path = "./.env";
 
 
 int main(){
+    try {
+        env.load_dotenv(env_path); 
+        
+        GLOBAL_EXPECTED_API_KEY = env["API_KEY"];
+
+        if (GLOBAL_EXPECTED_API_KEY.empty()) {
+            cerr << "ERRO: A chave API_KEY não foi encontrada ou está vazia no arquivo .env" << endl;
+        }
+        else{
+             cout << "Carregando .env do caminho absoluto: " << endl;
+        }
+
+    } catch (const exception& e) {
+        cerr << "Erro ao carregar o arquivo .env: " << e.what() << endl;
+        return 1; 
+    }
     crow::SimpleApp app; // criando uma instância do aplicativo
         try{
             mainRoute(app);
